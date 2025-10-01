BUGFIX: corrected sign GRAD-GRAD matrix in system

CTG/Assembler.py
@@ -39,7 +39,8 @@ class AssemblerWave:
         L_mat = self.space_time_fe.matrix["L"]
         D_mat = self.space_time_fe.matrix["D_t"]
         M_xt = self.space_time_fe.matrix["M"]
-        A0_no_bc = scipy.sparse.block_array([[D_mat, -M_xt], [-L_mat, D_mat]])
+        A0_no_bc = scipy.sparse.block_array([[D_mat, -M_xt], [L_mat, D_mat]])
+        # L_mat goes with + sign becaus eit actualkly represent grad-grad term
         # Right hand side vector
         xt_dofs = self.space_time_fe.dofs
         rhs0 = M_xt.dot(exact_rhs_0(xt_dofs))