e5ea9fd034ff762b9473246850bc858557c039c5
andreascaglioni <andrea.c.scaglioni@gmail.com>
2025-10-06 19:00:00 +0200

    restructure error computation: input 1 comp u or v

--- CTG/error.py
+++ CTG/error.py
@@ -1,5 +1,5 @@
 import numpy as np
-from CTG.FE_spaces import SpaceFE, TimeFE
+from CTG.FE_spaces import SpaceFE, TimeFE, SpaceTimeFE
 from CTG.utils import cart_prod_coords
 
 
@@ -11,20 +11,20 @@ from scipy.interpolate import griddata
 from math import sqrt
 
 
-def compute_error_slab(sol_slab, exact_sol, space_fe, time_fe, err_type_x, err_type_t):
+def compute_error_slab(sol_slab, exact_sol, space_time_fe, err_type_x, err_type_t):
 
     # refine Time
-    msh_t = time_fe.mesh
-    msh_t_ref = mesh.refine(msh_t)[0]
-    p_t_trial = time_fe.V_trial.element.basix_element.degree
-    V_t_ref = fem.functionspace(msh_t_ref, ("Lagrange", p_t_trial))
     
+    msh_t = space_time_fe.time_fe.mesh
+    msh_t_ref = mesh.refine(msh_t)[0]
+    p_t_trial = space_time_fe.time_fe.V.element.basix_element.degree
+    V_t_ref = fem.functionspace(msh_t_ref, ("Lagrange", p_t_trial))
     time_fe_ref = TimeFE(msh_t_ref, V_t_ref)
 
     # refine Space
-    msh_x = space_fe.mesh
+    msh_x = space_time_fe.space_fe.mesh
     msh_x_ref = mesh.refine(msh_x)[0]
-    p_Space = space_fe.V.element.basix_element.degree
+    p_Space = space_time_fe.space_fe.V.element.basix_element.degree
     V_x_ref = fem.functionspace(msh_x_ref, ("Lagrange", p_Space))
     space_fe_ref = SpaceFE(V_x_ref)
 
@@ -34,7 +34,7 @@ def compute_error_slab(sol_slab, exact_sol, space_fe, time_fe, err_type_x, err_t
 
     # Interpolate numerical sol using griddata (linear interpolation)
     # TODO works only for Lagrangian FE
-    coarse_coords = cart_prod_coords(time_fe.dofs_trial, space_fe.dofs)
+    coarse_coords = space_time_fe.dofs
     sol_slab_ref = griddata(
         coarse_coords, sol_slab, fine_coords, method="linear", fill_value=0.0
     )
@@ -50,17 +50,16 @@ def compute_error_slab(sol_slab, exact_sol, space_fe, time_fe, err_type_x, err_t
     err_fun_ref = ex_sol_ref - sol_slab_ref
 
     if err_type_t == "l2":
-        ip_tx = scipy.sparse.kron(time_fe_ref.matrix["mass"], ip_space_ref)
-        err = sqrt(ip_tx.dot(err_fun_ref).dot(err_fun_ref))
-        norm_u = sqrt(ip_tx.dot(ex_sol_ref).dot(ex_sol_ref))
+        ip_tx = scipy.sparse.kron(time_fe_ref.matrix["mass_err"], ip_space_ref)
+        err = sqrt((ip_tx @ err_fun_ref) @ err_fun_ref )   # ip_tx.dot(err_fun_ref).dot(err_fun_ref))
+        norm_u = sqrt((ip_tx @ (sol_slab_ref)) @ (sol_slab_ref))
     elif err_type_t == "linf":
         err = -1.0
         norm_u = -1.0
-        for i, t in enumerate(time_fe.dofs_trial):
-            coords_u_t = ex_sol_ref[i * space_fe_ref.n_dofs : (i + 1) * space_fe_ref.n_dofs]
+        for i in range(time_fe_ref.n_dofs):
+            coords_u_t = sol_slab_ref[i * space_fe_ref.n_dofs : (i + 1) * space_fe_ref.n_dofs]
             norm_u_t = sqrt(ip_space_ref.dot(coords_u_t).dot(coords_u_t))
             norm_u = max(norm_u, norm_u_t)
-
             coords_err_t = err_fun_ref[i * space_fe_ref.n_dofs : (i + 1) * space_fe_ref.n_dofs]
             err_t = sqrt(ip_space_ref.dot(coords_err_t).dot(coords_err_t))
             err = max(err, err_t)
@@ -69,31 +68,26 @@ def compute_error_slab(sol_slab, exact_sol, space_fe, time_fe, err_type_x, err_t
     return err, norm_u
 
 
-def compute_err(comm, order_t, err_type_x, err_type_t, time_slabs, space_fe, sol_slabs, sol_slabs_ref, space_fe_ref = None, order_t_ref = None, time_slabs_ref = None):
+def compute_err(comm, 
+                order_t, 
+                err_type_x, 
+                err_type_t, 
+                time_slabs, 
+                space_fe, 
+                sol_slabs, 
+                sol_exa):
     
-    # exact_sol_u can be either Callable (analytic) or fem.Function (reference)
-    if type(sol_slabs_ref) is fem.Function:
-        assert (space_fe_ref is not None) and (order_t_ref is not None) and (time_slabs_ref is not None)
-        raise NotImplementedError("Reference solution as fem.Function is not implemented yet.")
 
     err_slabs = -1.0 * np.ones(len(time_slabs))
     norm_u_slabs = -1.0 * np.ones_like(err_slabs)
     for i, slab in enumerate(time_slabs):
-        # Furrent time FE
+        # Current time FE
         msh_t = mesh.create_interval(comm, 1, [slab[0], slab[1]])
         V_t_trial = fem.functionspace(msh_t, ("Lagrange", order_t))
         time_fe = TimeFE(msh_t, V_t_trial)
-
-        # Extract u curr slab
+        space_time_fe = SpaceTimeFE(space_fe, time_fe)
         X = sol_slabs[i]
-        u = X[:int(X.size/2)]
-
-        # Get dofs sol_slab_ref for current slab
-        if type(sol_slabs_ref) is fem.Function:
-            X = sol_slabs[i]
-        u = X[:int(X.size/2)]
-
-        err_slabs[i], norm_u_slabs[i] = compute_error_slab(u, sol_slabs_ref, space_fe, time_fe, err_type_x, err_type_t)
+        err_slabs[i], norm_u_slabs[i] = compute_error_slab(X, sol_exa, space_time_fe, err_type_x, err_type_t)
 
 
     if err_type_t == "linf":
@@ -106,4 +100,5 @@ def compute_err(comm, order_t, err_type_x, err_type_t, time_slabs, space_fe, sol
     else:
         raise ValueError(f"Unknown error type in time: {err_type_t}")
     total_rel_err = total_err / total_norm_u
+
     return total_err, total_rel_err, err_slabs, norm_u_slabs
