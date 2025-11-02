d6eb337508b7d538f493d7e826d0f59af4988a97
andreascaglioni <andrea.c.scaglioni@gmail.com>
2025-11-02 12:58:23 +0100

    fix example WE conv dt

--- examples/WE_CTG_conv_dt.py
+++ examples/WE_CTG_conv_dt.py
@@ -9,10 +9,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
+from ctg.plotting import compute_rate, float_f
+
 sys.path.insert(0, "./")
 from ctg.ctg_solver import CTGSolver
 from ctg.error import compute_err
-from ctg.post_process import float_f, compute_rate, compute_energy_tt
+from ctg.post_process import compute_energy_tt
 from ctg.config import load_config
 
 
@@ -21,9 +23,8 @@ if __name__ == "__main__":
     np.set_printoptions(formatter={"float_kind": float_f})
 
     # PARAMETERS
-    data_dir = Path(__file__).parent.parent / "data"
-    data_file = data_dir / "data_swe.yaml"
-    config = load_config(data_file)
+    data_file = Path("examples/data_examples/data_we.yaml")
+    cfg = load_config(data_file)
 
     ddt = 2.0 ** (-np.arange(2, 7))  # time step sizes
 
@@ -38,35 +39,35 @@ if __name__ == "__main__":
     for i, dt in enumerate(ddt):
         print("\nDT = ", dt)
         # numerics_params["t_slab_size"] = ddt[i]
-        config.numerics.t_slab_size = ddt[i]
+        cfg.numerics.t_slab_size = ddt[i]
 
         # Compute
-        ctg_solver = CTGSolver(config.numerics)
-        sol_slabs, time_slabs, space_time_fe, total_n_dofs = ctg_solver.run(config.physics)
+        ctg_solver = CTGSolver(cfg.numerics)
+        sol_slabs, time_slabs, space_time_fe, total_n_dofs = ctg_solver.run(cfg.physics)
 
         # Error
         n_scalar = space_time_fe.n_dofs
         sol_u = [s[:n_scalar] for s in sol_slabs]
         sol_v = [s[n_scalar:] for s in sol_slabs]
         err_u, _, _, _ = compute_err(
-            config.numerics.comm,
-            config.numerics.order_t,
+            cfg.numerics.comm,
+            cfg.numerics.order_t,
             "h1",
             "l2",
             time_slabs,
             space_time_fe.space_fe,
             sol_u,
-            config.physics.exact_sol_u,
+            cfg.physics.exact_sol_u,
         )
         err_v, _, _, _ = compute_err(
-            config.numerics.comm,
-            config.numerics.order_t,
+            cfg.numerics.comm,
+            cfg.numerics.order_t,
             "l2",
             "l2",
             time_slabs,
             space_time_fe.space_fe,
             sol_v,
-            config.physics.exact_sol_v,
+            cfg.physics.exact_sol_v,
         )
         err[i] = err_u + err_v
         print("Total error", float_f(err[i]))
@@ -92,7 +93,7 @@ if __name__ == "__main__":
 
     plt.figure()
     for i, dt in enumerate(ddt):
-        tt = np.linspace(config.physics.start_time, config.physics.end_time, int(1 / dt))
+        tt = np.linspace(cfg.physics.start_time, cfg.physics.end_time, int(1 / dt))
         color_intensity = (i + 1) / len(ddt)
         col = plt.get_cmap("Blues")(0.3 + 0.7 * color_intensity)
         plt.plot(tt, EE[i], "o-", linewidth=2, label="dt = " + str(dt), color=col)
